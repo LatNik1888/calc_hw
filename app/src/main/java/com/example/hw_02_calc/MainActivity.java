1a52e3b9f1a397a7cf4f0dd7af89dff02f95e488
NikLat1888 <detr1889@gmail.com>
2021-06-03 10:07:25 +0300

    calc ops

--- app/src/main/java/com/example/hw_02_calc/MainActivity.java
+++ app/src/main/java/com/example/hw_02_calc/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
+//Код неоптимальный, но время, увы, поджимает. Все размеры в ресурсы занесу к следующему дз, там как раз это надо сделать. Плюс, попробую покурить мануалы по Data Binding. Наткнулся на него, выглядит интересно
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText inputField;
     Button btn0;
